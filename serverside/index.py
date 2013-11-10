65ad7136da5d51cef7c2c4b6307d38d652139c15
Stephen Sulivan <them@wirelessprvnat-172-16-241-188.near.illinois.edu>
2013-11-09 18:07:15 -0600

    Inital commit

--- serverside/index.py
+++ serverside/index.py
@@ -1,25 +1,41 @@
-#import praw
+import praw
 from threading import Thread
 from SocketServer import ThreadingMixIn
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from urlparse import parse_qs
+from urlparse import parse_qs,urlparse
 from cgi import parse_header, parse_multipart
+import sys
+from pprint import pprint
+import json
 
 
 class Handler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		self.send_response(200)
-		self.send_header("Content-type", "text/plain")
+		self.send_header('Access-Control-Allow-Origin', '*')
 		self.end_headers()
-		if self.path=="/slutty":
-			print "you\'re at slutty"
-			self.wfile.write("<hr>")
-		else:
-			print "you\'re at home!"
-		#r = praw.Reddit(user_agent='redditrap')
-		#submissions = r.get_subreddit('funny').get_hot(limit=5)
-		#for post in submissions:
-		#	self.wfile.write(str(post)+"\n")
+		print parse_qs(self.path[self.path.find('?')+1:])
+		if self.path=="/sub":
+			print "Submit to Reddit"
+			self.wfile.write("Submit a Reddit Link!")
+		elif self.path=="/posts":
+			print "Getting Reddit Posts\n\n"
+			r = praw.Reddit(user_agent='redditwrap')
+			submissions = r.get_subreddit('funny').get_hot(limit=5)
+			res = []
+			for post in submissions:
+				res.append({
+					'num_comments': post.num_comments,
+					'url': post.url,
+					'title': post.title,
+					'user': str(post.author),
+					'subreddit': str(post.subreddit),
+					'time': post.created_utc,
+					'nsfw': post.over_18
+				})
+			pprint(json.dump(res))
+		elif self.path=="/kill":
+			sys.exit(0)
 
 	def do_POST(self):
 		postvars = self.parse_POST()
@@ -44,8 +60,8 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 	pass
 
 def serve_on_port(port):
-	server = ThreadingHTTPServer(("python-redditwrap.rhcloud.com",port), Handler)
+	server = ThreadingHTTPServer(("172.16.96.184",port), Handler)
 	server.serve_forever()
 
 Thread(target=serve_on_port, args=[1111]).start()
-serve_on_port(80)
+serve_on_port(2222)
